64a2917d92e469954e841e02b738e72753628a54
jang sung ho <wkdtjdgh0429@naver.com>
2020-10-31 20:06:10 +0900

    Add:clock make2

--- text/clock.js
+++ text/clock.js
@@ -1,16 +1,17 @@
-const clockContainer = document.querySelector(".js-clock"),
-clockTitle = clockContainer.querySelector("h1");
- 
- function getTime(){
-     const date = new Date();
-     const minutes = date.getMinutes();
-     const hours = date.getHours();
-     const seconds = date.getSeconds();
-     clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
- }
+const clockcontainer = document.querySelector(".js-clock"),
+colckTitle = clockcontainer.querySelector("h1")
+
+function getTime(){
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    colckTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+     
+}
 
 function init(){
     getTime();
+    setInterval(getTime, 1000);
 }
-
 init();
