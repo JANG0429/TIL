fd9ecc78fa51acbbc642f2c00808ada6b30f7925
jang sung ho <wkdtjdgh0429@naver.com>
2020-11-01 20:17:07 +0900

    Add: saving userName part one

---
+++ text/gretting.js
@@ -0,0 +1,29 @@
+const form = document.querySelector(".js-form"),
+input = form.querySelector("input"),
+greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currntUser",
+ SHOWING_CN = "showing";
+
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText =`Hell ${text}`
+}
+
+function loadName(){
+const currentUser = localStorage.getItem(USER_LS);
+if(currentUser === null){
+
+} else {
+paintGreeting(currentUser);
+}
+}
+
+
+
+function init() {
+    loadName();
+}
+
+init();
