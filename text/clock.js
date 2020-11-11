2bd5b30684a4aefd7c384cdcb307335f40de4cbf
jang sung ho <wkdtjdgh0429@naver.com>
2020-11-11 18:52:47 +0900

    Add:todoList part one!

--- text/clock.js
+++ text/clock.js
@@ -1,16 +1,18 @@
-const clockContainer = document.querySelector(".js-clock"),
-clcokTitle = clockContainer.querySelector("h1")
+const clockcontainer = document.querySelector(".js-clock"),
+clockTitle = clockcontainer.querySelector("h1");
 
-function getTime(){
+function getTime() {
     const date = new Date();
-    const minutes = date.getMinutes();
+    const minutes =date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-clcokTitle.innerText =`${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}` 
+    clockTitle.innerText =`${hours < 10 ? `0${hours}`: hours}:${minutes < 10 ? `0${minutes}`: minutes}:${seconds < 10 ? `0${seconds}`: seconds}`;
+    
 }
 
-function init(){
+function init() {
     getTime();
-    setInterval(getTime, 1000);
+    setInterval(getTime,1000)
+    
 }
 init();
