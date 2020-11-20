84b951bc845e751cc0eee77d63a512fcad53b616
jang sung ho <wkdtjdgh0429@naver.com>
2020-11-20 23:05:42 +0900

    Add: if문복습과 예제문제 다시 풀고 설명

--- testt/test.js
+++ testt/test.js
@@ -1,7 +1,34 @@
-function solution(arr){
-    var arr = [1,2,3,4,5];
-    return arr[0]
+function daysInMonth( month ){
+    // 여기에 switch를 이용해 각 달의 날짜수를 반환하는 프로그램을 작성하세요.
+  switch(month) {
+    case 2:
+            return 28;
+        break;
+    case 4:
+            return 30;
+        break;
+    case 6:
+            return 30;
+        break;
+    case 9:
+            return 30;
+        break;
+    case 11:
+            return 30;
+        break;
+    default :
+            return 31;
+  }
 }
 
-var result = solution([1,2,3,4,5])
-console.log(result)
+
+
+
+ var age = 25;
+
+ if(age < 20){
+     console.log("20세 미만입니다. ");
+     
+ } else {
+     console.log("20세 이상입니다.");
+ }
