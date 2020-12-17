c29101fd401249e2beda46ca5a6bc4c41cee66d5
sungho <wkdtjdgh0429@naver.com>
2020-12-17 15:24:59 +0900

    add: HTML 복습

--- testt/test.js
+++ testt/test.js
@@ -1,34 +1,11 @@
-function daysInMonth( month ){
-    // 여기에 switch를 이용해 각 달의 날짜수를 반환하는 프로그램을 작성하세요.
-  switch(month) {
-    case 2:
-            return 28;
-        break;
-    case 4:
-            return 30;
-        break;
-    case 6:
-            return 30;
-        break;
-    case 9:
-            return 30;
-        break;
-    case 11:
-            return 30;
-        break;
-    default :
-            return 31;
-  }
+function solution(array, commands) {
+    return commands.map(command => {
+        const [sPosition, ePosition, position] = command
+        const newArray = array
+            .filter((value, fIndex) => fIndex >= sPosition - 1 && fIndex <= ePosition - 1)
+            .sort((a,b) => a - b)    
+
+        return newArray[position - 1]
+    })
 }
 
-
-
-
- var age = 25;
-
- if(age < 20){
-     console.log("20세 미만입니다. ");
-     
- } else {
-     console.log("20세 이상입니다.");
- }
--- text/tttt.js
+++
@@ -1,4 +0,0 @@
-var str="3.141592는 Pi의 근사값입니다."
-var pi_int = 3.14;
-
-console.log( pi_int);
